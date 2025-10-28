360d7f70a0073bc83f6153299bf097073faa62b2
Rodrigo Romero Corvera <rocobros21@gmail.com>
2025-10-27 22:44:04 -0600

    Add files via upload

---
+++ MiCarpeta/main.py
@@ -0,0 +1,5 @@
+print("Calculadora de suma:")
+primero = float(input("Ingresa el primer numero: "))
+segundo = float(input("Ingresa el segundo numero: "))
+total = primero + segundo
+print(f"El total es: {total}")
